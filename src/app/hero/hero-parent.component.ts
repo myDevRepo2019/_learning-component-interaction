9e7234a57dabcb2aeb1bf187f1f4042587732260
Marcel <m.yildiz@alpencode.com>
2019-01-01 08:55:41 +0100

    fat arrows

--- src/app/hero/hero-parent.component.ts
+++ src/app/hero/hero-parent.component.ts
@@ -11,5 +11,23 @@ import { HEROES } from '../hero';
 
 export class HeroParentComponent {
     heroes = HEROES;
-    master = 'Master'; 
+    master = 'Master';
+
+    constructor() {
+
+        // Fat Arrow usage
+        // Original JS usage
+        const add = function(a, b) { return a + b; };
+        console.log(add(1, 2));
+
+        // new ES6 fat arrow usage
+        const add1 = (a, b) => { return a + b; };
+        console.log(add1(5, 7));
+
+        // further simplified fat arrow usage, if code is in one line
+        const add2 = (a, b) =>  a + b;
+        console.log(add1(3, 5));
+
+    }
+
 }
